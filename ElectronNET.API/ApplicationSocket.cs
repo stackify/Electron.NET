258a8823bfdff1eb47faed741a89293fa0c88e10
Daniel Gidman <danatcofo@gmail.com>
2021-04-26 16:57:06 -0500

    Add a direct connection to the socket

---
+++ ElectronNET.API/ApplicationSocket.cs
@@ -0,0 +1,15 @@
+﻿using Quobject.SocketIoClientDotNet.Client;
+
+namespace ElectronNET.API
+{
+    /// <summary>
+    /// Wrapper for the underlying Socket connection
+    /// </summary>
+    public class ApplicationSocket
+    {
+        /// <summary>
+        /// Socket used to communicate with main.js
+        /// </summary>
+        public Socket Socket { get; internal set; }
+    }
+}
